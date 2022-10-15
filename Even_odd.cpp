e8b78bb45c7ea3d53f6e97594c68eb92ac8121ca
Siddharth Shrivastav <44272028+damascus8@users.noreply.github.com>
2022-10-15 13:46:15 +0530

    Update Even_odd.cpp

--- Even_odd.cpp
+++ Even_odd.cpp
@@ -1,1 +1,40 @@
 //create a code to check even odd numbers.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int n=0;
+
+int i;
+
+cout<<"Enter the end limit";
+
+cin>>n;
+
+for(i=0;i<=n;i++)
+
+{
+
+    if(i%2==0){
+
+        printf("number is even & is = %d\n",i);
+
+    }
+
+    else{
+
+        printf("number is odd and is = %d\n",i);
+
+    }
+
+    
+
+}
+
+    return 0;
+
+}
